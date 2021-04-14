7c62ffada49353a84f9360a25b55f9076b7a6c28
yuehhan <yuehwhan@gmail.com>
2021-04-14 03:47:25 -0400

    reverse print statement

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -6,7 +6,6 @@ import Card from '../../shared/components/UIElements/Card';
 import './UserItem.css';
 
 const UserItem = props => {
-  console.log(props)
   return (
     <li className="user-item">
       <Card className="user-item__content">
